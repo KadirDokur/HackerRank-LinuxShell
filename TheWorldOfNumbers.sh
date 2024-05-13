5e7758f7569e695fb3240851889b5439fc19a741
Nithrone <kadir.dokur@bil.omu.edu.tr>
2024-05-13 13:11:24 +0300

    modified:   TheWorldOfNumbers.sh

--- TheWorldOfNumbers.sh
+++ TheWorldOfNumbers.sh
@@ -18,8 +18,14 @@ elif ((number2 == 0)); then
     exit 1
 fi
 
+echo $(( number1 + number2 ))
+echo $(( number1 - number2 ))
+echo $(( number1 * number2 ))
+echo $(( number1 / number2 ))
 
-expr $number1 + $number2
-expr $number1 - $number2
-expr $number1 \* $number2
-expr $number1 / $number2
+# The codes below were mine and they gave the exact same output as the problem requested.
+# However it didn't pass the first 2 test cases even though the requirement were met. Weird...
+# expr $number1 + $number2
+# expr $number1 - $number2
+# expr $number1 \* $number2
+# expr $number1 / $number2
