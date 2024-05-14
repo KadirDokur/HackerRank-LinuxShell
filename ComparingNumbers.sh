ae45c83566c3a334b8c0fb9b5228a07bbabe43b4
Nithrone <kadir.dokur@bil.omu.edu.tr>
2024-05-14 10:04:55 +0300

    new file:   ComparingNumbers.sh

---
+++ ComparingNumbers.sh
@@ -0,0 +1,17 @@
+# Given two integers, X and Y, identify whether X>Y or X<Y or X=Y.
+
+# Exactly one of the following lines:
+# - X is less than Y
+# - X is greater than Y
+# - X is equal to Y
+
+read x
+read y
+
+if (( $x > $y )); then
+    echo "X is greater than Y"
+elif (( $x == $y )); then
+    echo "X is equal to Y"
+else
+    echo "X is less than Y"
+fi
